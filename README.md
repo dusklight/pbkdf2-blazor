d9b4a306eced25fe2e5c520d97f14c5beaecd7f7
dusklight <dusklight@users.noreply.github.com>
2021-11-08 23:02:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PBKDF2 Tool in Blazor
+
+[![.NET](https://github.com/dusklight/pbkdf2-blazor/actions/workflows/dotnet.yml/badge.svg)](https://github.com/dusklight/pbkdf2-blazor/actions/workflows/dotnet.yml)
+
+A simple app written in Blazor for generating PBKDF2 hash.
+
+Refer to this [blog post](https://blog.dusklight.com/2021/11/pbkdf2-tool-in-blazor.html) for more information.
+
+![Screenshot of PBKDF2 Tool](Pbkdf2Form-Screenshot.png)
---
+++ src/PBKDF2BlazorServerApp/Features/Pbkdf2/Pbkdf2Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PBKDF2BlazorServerApp.Features.Pbkdf2
+{
+    public class Pbkdf2Model
+    {
+        [Required]
+        public string TextToHash { get; set; }
+
+        [Required]
+        public string SaltBase64 { get; set; }
+
+        public int? SaltSizeBytes { get; set; } = 16; // Keycloak uses 16 bytes as default.
+
+        [Required]
+        public int IterationCount { get; set; } = 27500; // Keycloak uses 27500 iterations as default.
+
+        [Required]
+        public int HashSizeBytes { get; set; } = 64; // Keycloak uses 64 bytes (512 bits) as default.
+    }
+}
